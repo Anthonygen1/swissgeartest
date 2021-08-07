67da8c99b7e320e55d1da33c2a736c38b6624c3d
Anthonygen1 <argenova@comcast.net>
2021-08-07 13:51:39 -0400

    Rendered to screen

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
 const axios = require('axios');
 const url = 'https://rma.swissgear.com/api/interview-endpoint';
 
-let products = {};
 
-axios({
-    method: 'get',
-    url: url,
-    responseType: 'stream'
-})
+axios.get(url)
     .then(data => {
         const items = data.data.data;
         // console.log(items)
-        dostuff(items)
+        items.forEach(item => {
+            createCard(item)
+            console.log(items)
+        })
     })
     .catch(err => console.log(err))
 
 
-function dostuff (arg) {
-    console.log(arg)
+function createCard(number) {
+    // creates card with child data
+    const card = document.createElement('li');
+    card.className = "card";
 
-    arg.forEach((item) => {
-        products = item
-        // console.log(products)
-        var h1 = document.createElement('div');
-        h1.textContent = products.name
-        var app = document.querySelector('div');
-        app.append(h1)
-    })
+    const img = document.createElement('img');
+    img.src = number.base_image;
+    img.className = "img";
+    img.id = "test";
+
+    const name = document.createElement('a');
+    name.innerText = number.name;
+    name.className = "title";
+
+    const price = document.createElement('h3');
+    price.innerText = number.price;
+    price.className = "price";
+
+    card.append(img);
+    card.append(name);
+    card.append(price);
+    appendToTable(card)
+    return card
 }
+
+function appendToTable (data) {
+    // appends card to table
+    const table = document.getElementsByClassName('table')[0];
+    table.appendChild(data)
+
+    return table
+}
+
 
 
